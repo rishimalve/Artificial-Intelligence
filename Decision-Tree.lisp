51cb7ad3d27cc119cb5e0790b10a0db2b6c7e276
rishimalve <47190476+rishimalve@users.noreply.github.com>
2019-09-07 20:54:48 -0400

    Add files via upload

---
+++ Decision-Tree.lisp
@@ -0,0 +1,256 @@
+(setf *print-level* nil)
+(setf *print-length* nil)
+(setf *print-lines* nil)
+
+
+(defparameter *restaurant-examples*
+'(((alternative t) (has-bar nil) (open-friday nil) (hungry t) (patrons some) (price expensive) (raining nil) (reservation t) (type french) (estimated-wait-time 0-10) (:label t))
+((alternative t) (has-bar nil) (open-friday nil) (hungry t) (patrons full) (price cheap) (raining nil) (reservation nil) (type thai) (estimated-wait-time 30-60) (:label nil))
+((alternative nil) (has-bar t) (open-friday nil) (hungry nil) (patrons some) (price cheap) (raining nil) (reservation nil) (type burger) (estimated-wait-time 0-10) (:label t))
+((alternative t) (has-bar nil) (open-friday t) (hungry t) (patrons full) (price cheap) (raining nil) (reservation nil) (type thai) (estimated-wait-time 10-30) (:label t))
+((alternative t) (has-bar nil) (open-friday t) (hungry nil) (patrons full) (price expensive) (raining nil) (reservation t) (type french) (estimated-wait-time >60) (:label nil))
+((alternative nil) (has-bar t) (open-friday nil) (hungry t) (patrons some) (price medium) (raining t) (reservation t) (type italian) (estimated-wait-time 0-10) (:label t))
+((alternative nil) (has-bar t) (open-friday nil) (hungry nil) (patrons none) (price cheap) (raining t) (reservation nil) (type burger) (estimated-wait-time 0-10) (:label nil))
+((alternative nil) (has-bar nil) (open-friday nil) (hungry t) (patrons some) (price medium) (raining t) (reservation t) (type thai) (estimated-wait-time 0-10) (:label t))
+((alternative nil) (has-bar t) (open-friday t) (hungry nil) (patrons full) (price cheap) (raining t) (reservation nil) (type burger) (estimated-wait-time >60) (:label nil))
+((alternative t) (has-bar t) (open-friday t) (hungry t) (patrons full) (price expensive) (raining nil) (reservation t) (type italian) (estimated-wait-time 10-30) (:label nil))
+((alternative nil) (has-bar nil) (open-friday nil) (hungry nil) (patrons none) (price cheap) (raining nil) (reservation nil) (type thai) (estimated-wait-time 0-10) (:label nil))
+((alternative t) (has-bar t) (open-friday t) (hungry t) (patrons full) (price cheap) (raining nil) (reservation nil) (type burger) (estimated-wait-time 30-60) (:label t)))
+"A list of examples for the Restaurant problem.
+Examples take the form of ((attr1 val) (attr2 val) ... (:label label))")
+
+
+(defparameter *restaurant-trial-examples*
+'(((alternative t) (has-bar nil) (open-friday nil) (hungry t) (patrons full) (price cheap) (raining nil) (reservation nil) (type thai) (estimated-wait-time 30-60) (:label nil))
+((alternative nil) (has-bar t) (open-friday nil) (hungry nil) (patrons some) (price cheap) (raining nil) (reservation nil) (type burger) (estimated-wait-time 0-10) (:label t))
+((alternative t) (has-bar nil) (open-friday t) (hungry nil) (patrons full) (price expensive) (raining nil) (reservation t) (type french) (estimated-wait-time >60) (:label nil))
+((alternative nil) (has-bar nil) (open-friday nil) (hungry t) (patrons some) (price medium) (raining t) (reservation t) (type thai) (estimated-wait-time 0-10) (:label t))
+((alternative nil) (has-bar nil) (open-friday nil) (hungry nil) (patrons none) (price cheap) (raining nil) (reservation nil) (type thai) (estimated-wait-time 0-10) (:label nil))
+((alternative t) (has-bar t) (open-friday t) (hungry t) (patrons full) (price cheap) (raining nil) (reservation nil) (type burger) (estimated-wait-time 30-60) (:label t))))
+
+
+(defparameter *restaurant-feature-defs*
+'((alternative t nil)
+(has-bar t nil)
+(open-friday t nil)
+(hungry t nil)
+(patrons none some full)
+(price cheap medium expensive)
+(raining t nil)
+(reservation t nil)
+(type french thai burger italian)
+(estimated-wait-time 0-10 10-30 30-60 >60))
+"A list of feature-defs for the Restaurant problem.
+feature defs take the form (feature val1 val2 val3 ...)")
+
+
+(defparameter *restaurant-labels*
+'(t nil)
+"label Values for the Restaurant problem")
+
+
+(defun feature-val-for-example (example feature)
+  "Returns the value of a given feature as stored in an example"
+  (second (assoc feature example)))
+
+
+(defun label-of-example (example)
+  "Returns the label of an example, as stored in the example"
+  (second (assoc :label example)))
+
+
+(defun find-label-for-example (example decision-tree)
+  "Given a decision tree and an example, uses the decision tree
+    to determine the expected label for the example"
+  (if (equalp (first decision-tree) :label)
+      (second decision-tree) ;; return the label
+    (find-label-for-example example
+			    (second (assoc
+				     (feature-val-for-example example (first decision-tree))
+				     (rest decision-tree))))))
+
+(defun examples-have-same-label-p (examples)
+  "Returns t if all examples have the same label, else returns nil"
+  (let ((label (label-of-example (first examples))))
+    (dolist (example examples t)
+      (when (not (equalp label (label-of-example example)))
+	(return nil)))))
+
+
+(defun num-examples-with-label (examples label)
+  "Returns the number of examples with a given label"
+  (count-if #'(lambda (example) (equalp 
+				 (label-of-example example) label)) examples))
+         
+
+(defun most-common (elts &key (test #'equalp))
+  "Returns the elt in the list elts that appears the most often;
+     ties are broken arbitrarily, probably by picking the elt 
+     which appeared earliest. Two elts are considered to be
+     the same if they pass the :test (by default, equalp)"
+  (let* ((unique-elts (remove-duplicates elts :test test))
+	 (unique-nums (mapcar #'(lambda (x) (count x elts :test test)) unique-elts))
+	 (best-elt (first unique-elts))
+	 (best-num (first unique-nums)))
+    (mapc #'(lambda (elt num) (when (> num best-num) 
+				(setf best-elt elt) (setf best-num num)))
+	  (rest unique-elts) (rest unique-nums))
+    best-elt))
+
+
+(defun normalize (numbers)
+  "Normalizes a list of numbers by dividing them by their sum.  If
+    the numbers are all 0.0, normalize just returns the original numbers."
+  (let ((sum (apply #'+ numbers)))
+    (if (= sum 0.0) numbers 
+      (mapcar #'(lambda (num) (/ num sum)) numbers))))
+
+
+(defun num-examples-with-feature-val (examples feature value)
+  "Returns the number of examples with a given value for some feature"
+  (count-if #'(lambda (example) (equalp
+				 (feature-val-for-example example feature) value)) examples))
+
+
+(defun examples-with-feature-val (examples feature val)
+  "Returns the examples whose feature has value val"
+  (remove-if-not #'(lambda (x) (equalp (feature-val-for-example x feature) val)) examples))
+
+
+(defun unique (lsts &aux (h (make-hash-table :test 'equal)))
+  (loop :for lst :in lsts
+    :never (gethash lst h)
+    :do (setf (gethash lst h) t)))
+
+
+(defun most-common-label (examples)
+  "Returns the most common label found in examples"
+  (most-common (mapcar #'label-of-example examples)))
+
+(defun label-probabilities-for-feature-val (examples feature value labels)
+  "Returns a list, one item per label, 
+    of the percentage of examples with a given value for a given feature,
+    which belong to that label.  If they're all 0.0, you'll get back a list
+    of 0.0's, even though that doesn't add to 1.0 (which should be just fine).
+    If feature is nil, then this simply returns a list, one item per label, 
+    of the percentage of examples which belong to that label."
+
+  (let ((examples-with-feature-val
+	 (if (null feature) examples 
+	   (remove-if-not #'(lambda (x) (equalp 
+					 (feature-val-for-example x feature) value)) examples))))
+    (normalize
+     (mapcar #'(lambda (label) (/ 
+				(num-examples-with-label examples-with-feature-val label) 
+				(length labels)))
+	     labels))))
+
+(defun information (probabilities)
+"Given a list of probabilities, returns the information
+theoretically stored in that list"
+
+(let ((sum 0))
+    (dolist (p probabilities)
+      (if (equalp p 0)
+        (setf sum (+ sum p)) (setf sum (+ sum (* -1 (* p (log p 2)))))))sum))
+
+
+(defun remainder (feature-def examples labels)
+"Returns the sum, over each value that the feature can take on,
+of the probability of an example having that value, times the
+information content of the probabilities of various labels
+for all examples with that value."
+
+(let ((final-rem 0) (feature-probabilities '()))
+  (dolist (feature-value (cdr feature-def))
+    (setf feature-probabilities (label-probabilities-for-feature-val examples (car feature-def) feature-value labels))
+    (if (equalp (list-length examples) 0)
+      (setf final-rem (+ final-rem 0))
+      (setf final-rem (+ final-rem (* (information feature-probabilities) (/ (num-examples-with-feature-val examples (car feature-def) feature-value) (list-length examples)))))))final-rem))
+
+
+(defun choose-feature (feature-defs examples labels)
+"Returns the feature-def chosen from feature-defs
+that best divides up examples. This is done by
+picking the feature-def whose feature has
+the lowest remainder given the examples and labels. Ties
+for best are broken by picking the one considered earliest."
+
+(let ((remainder-list '()) (min-feature '()) (minrem 1000) )
+  (dolist (f feature-defs)
+    (setf remainder-list (append remainder-list (list (list f (remainder f examples labels))))))
+  (dolist (r remainder-list)
+    (if (< (nth 1 r) minrem)
+      (progn (setf minrem (nth 1 r)) (setf min-feature (nth 0 r)))))
+      min-feature))
+
+
+(let ((unique (gensym)))
+(defun build-decision-tree (examples feature-defs labels &optional (default unique))
+"Given a set of examples, a set of feature-defs, and a 
+set of label values, returns a decision tree which correctly 
+labelifies the examples. You can then run this decision tree
+on examples to see how it does by passing it to the function
+FIND-LABEL-FOR-EXAMPLE."
+
+(if (equalp default unique)
+(setf default (most-common-label examples)))
+
+(let ((tree '()) (f (choose-feature feature-defs examples labels)))
+(cond
+    ;;; base conditions
+    ((null examples)
+    (list ':label default))
+    ((examples-have-same-label-p examples)
+    (list ':label default))
+    ((null feature-defs)
+    (list ':label default))
+    (t
+      (setf tree (nconc (list (car f)) tree))
+      (dolist (v (cdr f))
+        (setf tree (nconc tree (list (list v (build-decision-tree (examples-with-feature-val examples (car f) v) (remove f feature-defs) labels))))))
+        tree)))))
+
+(defparameter *t* '())
+(setf *t* (build-decision-tree *restaurant-trial-examples* *restaurant-feature-defs* *restaurant-labels*))
+(print "***final tree***")
+(print *t*)
+;(print "***orginial data set labels***")
+;(print (mapcar #'label-of-example *restaurant-examples*))
+;(print "***tree generated labels***")
+;(print (mapcar #'(lambda (x) (find-label-for-example x *t*)) *restaurant-examples*))
+
+
+;;; The decision tree got wrong 3 labels in the generalization step.
+;;; Original labels:  (T NIL T T NIL T NIL T NIL NIL NIL T)
+;;; Generalization:   (T NIL T NIL NIL T NIL T T T NIL T)
+
+;;; Report:
+;;; The code implements the Decision Tree algorithm, which takes random samples of data and features and generalises it to a tree with a least possible travel which divides the dataset.
+;;; Execution of the code starts with the call to the function build-decision-tree which calls the choose-feature function which returns the next feature to be selected for building the tree.
+;;; choose-feature function uses the remainder and information functions to make decision on the next feature to be choses. This is the basic flow of the code.
+;;; 
+;;; The build-decision-tree function is on the same lines with the algorithm given in the class notes. Default label of the tree is choses as the most common label in the example dataset.
+;;; The function creates a local variable tree as an empty list at each recursive call which will be returned after each call ends. Then I've used nested conditional statements using cond block
+;;; which checks the below base condition of the recursion.
+;;;   1. example data set is empty
+;;;   2. all the examples in the dataset have the same label.
+;;;   3. we are no longer left with any feature definitions.
+;;; If any of the above conditions are not satisfied the function calls the build-decision-tree function recursively with reduced example and feature-def set. Otherwise base condition returns the 
+;;; correct label of the for the branch as per the algorithm.
+;;; 
+;;; Choose-feature function takes returns the feature which best divides the examples. It used remainder function which provides the remainder of each feature-def.
+;;; I've created a list called remainder-list which stores the feature-def and its remainder. A loop calls the remainder function for each of the features and the returned remainder is stored
+;;; in the remainder-list list along with its feature name. Then a loop traverses the entire remainder-list and chooses the feature with the least remainder which is returned to the build-decision-tree
+;;; function.
+;;; 
+;;; Remainder function returns the sum which is the product of the probability of the feature taking each value and the information content of each label of the feature. To calculate the
+;;; information of the labels, remainder function calls the information function internally. This function has two local variables, final-rem and feature-probabilities. List feature-probabilities
+;;; which stores the probability of the value of each label of the feature to the total label count of the feature. Final-rem is counted which is product of the information value returned 
+;;; for each label of the feature. This function internally uses helper function such as num-examples-with-feature-val, num-examples-with-label and examples-with-feature-val which are useful
+;;; in calculating the probabilities.
+;;; 
+;;; Information function takes the probabilities of each value of the feature and returns the sum i.e. the information stored in the feature to the remainder function. It loops over the 
+;;; list of the probabilities and calculates the sum of the product of each probability and the log of the probability to the base 2.
+;;; 
+;;; I have also checked working of this code on the department-room dataset and it gives the tree same as the tree which has been provided in the class notes.
